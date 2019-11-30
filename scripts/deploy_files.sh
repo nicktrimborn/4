8c785051b2c36845a08c85f9c302bdd5ab9acb4b
Nicolas Trimborn <nicolas.trimborn@tuni.fi>
2019-11-30 15:34:29 +0200

    balah

--- scripts/deploy_files.sh
+++ scripts/deploy_files.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-SDCARD=/media/$USER/056C-3FA9
-ID=056C-3FA9
+SDCARD=/media/$USER/57C5-A5A3
+ID=57C5-A5A3
 # udisksctl mount -b $(mount | grep ${ID} | cut -d" "  -f1)
 # udisksctl mount -b ${SDCARD}
 #rm -R .
 echo "Copying files"
 # cp -L ~/4/exercises/04/bitstream ${SDCARD}
 cp -L ~/4/exercises/05/bitstream ${SDCARD}
-chmod +x /4/exercises/07/statistics
-cp -L ~/4/exercises/07/statistics ${SDCARD}
+cp -L ~/4/exercises/07/statistics_app/statistics ${SDCARD}
+cp -L ~/4/exercises/07/statistics_app/measuring.sh ${SDCARD}
+cp -L ~/4/exercises/07/statistics_app/irqgen.csv ${SDCARD}
 cp -L ~/4/build/tmp/deploy/images/pynq/boot.bin ${SDCARD}
 cp -L ~/4/build/tmp/deploy/images/pynq/uImage ${SDCARD}
 cp -L ~/4/build/tmp/deploy/images/pynq/u-boot.img ${SDCARD}
@@ -16,6 +17,7 @@ cp -L ~/4/build/tmp/deploy/images/pynq/core-image-minimal-pynq.cpio.gz.u-boot ${
 cp -L ~/4/build/tmp/deploy/images/pynq/uEnv.txt ${SDCARD}
 cp -L ~/4/build/tmp/deploy/images/pynq/devicetree.dtb ${SDCARD}
 echo "Files Copied"
+chmod +x ~/4/exercises/07/statistics_app/statistics
 cd ~/4/
 echo "Unmounting"
 udisksctl unmount -b $(mount | grep ${ID}  | cut -d" "  -f1)
